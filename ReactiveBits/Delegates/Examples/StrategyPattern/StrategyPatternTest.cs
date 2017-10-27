95ec479e0bcfb10c03665c650ff2db172f75a236
Arialdo Martini <arialdo.martini@aduno-gruppe.ch>
2017-10-27 17:34:16 +0200

    Classic Strategy Pattern

---
+++ ReactiveBits/Delegates/Examples/StrategyPattern/StrategyPatternTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace ReactiveBits.Delegates.Examples.StrategyPattern
+{
+    public class StrategyPatternTest
+    {
+        public class LenghtComparer : IComparer<string>
+        {
+            public int Compare(string x, string y)
+            {
+                if (x.Length == y.Length) return 0;
+                return x.Length < y.Length ? 1 : 2;
+            }
+        }
+
+        [Fact]
+        public void classic_strategy_pattern()
+        {
+            var words = new[] {"short", "very very very long", "long enough"};
+            
+            words.ToList().Sort(new LenghtComparer());
+
+            words.Should().BeEquivalentTo("short", "long enough", "very very very long");
+        }
+    }
+}
