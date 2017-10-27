6b9cf79df7dea0a9bdf5e35a6bae851568f15e86
Arialdo Martini <arialdo.martini@aduno-gruppe.ch>
2017-10-27 16:05:20 +0200

    Anonymous delegates can be used as closures

--- ReactiveBits/Delegates/DelegatesTest.cs
+++ ReactiveBits/Delegates/DelegatesTest.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using FluentAssertions;
 using Xunit;
 using static ReactiveBits.Delegates.Delegates;
@@ -42,5 +43,49 @@ namespace ReactiveBits.Delegates
 
             actual.Should().BeTrue();
         }
+
+        [Fact]
+        public void closures_maintain_value_of_variables_defined_outside_their_scope()
+        {
+            Func<string> func = null;
+
+            {
+                var variable = "first value";
+
+                func = delegate()
+                {
+                    return variable;
+                };
+            }
+
+            func().Should().Be("first value");
+        }
+
+        [Fact]
+        public void closures_share_the_same_reference_to_captured_variables()
+        {
+            Func<string> func1 = null;
+            Func<string> func2 = null;
+
+            {
+                var variable = "first value";
+
+                func1 = delegate()
+                {
+                    variable = "second value";
+                    return variable;
+                };
+
+                func2 = delegate()
+                {
+                    return variable;
+                };
+            }
+
+            func2().Should().Be("first value");
+            func1().Should().Be("second value");
+            func1().Should().Be("second value");
+        }
+
     }
 }
