2913e161e3fa333b92c9212d8e1151d66eb35e31
Arialdo Martini <arialdo.martini@aduno-gruppe.ch>
2017-11-06 11:59:15 +0100

    Using yield and LINQ

---
+++ ReactiveBits/FunctionalProgramming/DeferredExecution/YieldExamples.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace ReactiveBits.FunctionalProgramming.DeferredExecution
+{
+    public class YieldExamples
+    {
+        public class SomeClass
+        {
+            public static IEnumerable<string> Coroutine()
+            {
+                yield return "foo";
+                yield return "bar";
+                yield return "baz";
+            }
+        }
+
+        [Fact]
+        public void coroutines_can_be_queried_with_LINQ()
+        {
+            var values = from v in SomeClass.Coroutine() select v;
+
+            values.Should().Contain("foo");
+            values.Should().Contain("bar");
+            values.Should().Contain("baz");
+        }
+    }
+}
