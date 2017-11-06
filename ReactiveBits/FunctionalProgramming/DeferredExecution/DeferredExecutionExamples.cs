b4cd572788e0cedf47e7d3d51ba47eea7c52b82d
Arialdo Martini <arialdo.martini@aduno-gruppe.ch>
2017-11-06 11:54:44 +0100

    LINQ queries are deferred and are executed on demand

---
+++ ReactiveBits/FunctionalProgramming/DeferredExecution/DeferredExecutionExamples.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+using Xunit;
+
+namespace ReactiveBits.FunctionalProgramming.DeferredExecution
+{
+    public class DeferredExecutionExamples
+    {
+        [Fact]
+        public void LINQ_queries_are_executed_on_demand()
+        {
+            var ints = new List<int> {1, 2, 3, 4, 5};
+
+            var even = from i in ints
+                where i % 2 == 0
+                select i;
+
+            ints.Add(6);
+
+            even.Should().Contain(6);
+        }
+    }
+}
