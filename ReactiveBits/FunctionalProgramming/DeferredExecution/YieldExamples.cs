f72ca2f73668bf03677abc8044c5be19009f8da4
Arialdo Martini <arialdo.martini@aduno-gruppe.ch>
2017-11-06 12:06:16 +0100

    Coroutines can be iterated

--- ReactiveBits/FunctionalProgramming/DeferredExecution/YieldExamples.cs
+++ ReactiveBits/FunctionalProgramming/DeferredExecution/YieldExamples.cs
@@ -15,6 +15,20 @@ namespace ReactiveBits.FunctionalProgramming.DeferredExecution
                 yield return "bar";
                 yield return "baz";
             }
+
+            public IEnumerable<string> AnotherCoroutine()
+            {
+                Step = 1;
+                yield return "foo";
+
+                Step = 2;
+                yield return "bar";
+
+                Step = 3;
+                yield return "baz";
+            }
+
+            public int Step { get; set; }
         }
 
         [Fact]
@@ -25,6 +39,29 @@ namespace ReactiveBits.FunctionalProgramming.DeferredExecution
             values.Should().Contain("foo");
             values.Should().Contain("bar");
             values.Should().Contain("baz");
+        }
+
+        [Fact]
+        public void coroutines_can_be_iterated()
+        {
+            var sut = new SomeClass();
+            sut.Step.Should().Be(0);
+
+            var iterator = sut.AnotherCoroutine().GetEnumerator();
+
+            iterator.MoveNext().Should().Be(true);
+            iterator.Current.Should().Be("foo");
+            sut.Step.Should().Be(1);
+            
+            iterator.MoveNext().Should().Be(true);
+            iterator.Current.Should().Be("bar");
+            sut.Step.Should().Be(2);
+            
+            iterator.MoveNext().Should().Be(true);
+            iterator.Current.Should().Be("baz");
+            sut.Step.Should().Be(3);
+
+            iterator.MoveNext().Should().Be(false);
         }
     }
 }
